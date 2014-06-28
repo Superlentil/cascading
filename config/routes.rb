9449e7a6a67f0f7f956307aabebbe30b63428914
Dingyu Zhou <dzhou@newfieldwireless.com>
2014-06-27 18:02:08 -0700

    [Develop]: Added model Article and added controller Articles.

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   #   resources :products
   resources :tiers
   resources :users
+  resources :articles
 
   # Example resource route with options:
   #   resources :products do
