f0a4922c2bf59a1c62cfc1b7f6f0dcd20002b134
Dingyu Zhou <dingyu.zhou@colorado.edu>
2014-08-20 00:33:04 -0700

    [Develop]: Bearly realize resize.

--- app/assets/javascripts/view/article/index/main.js
+++ app/assets/javascripts/view/article/index/main.js
@@ -2,9 +2,10 @@
 View.Article.Index.Main = Backbone.View.extend({
   initialize: function() {
     _.bindAll(this, "handleScroll");
+    _.bindAll(this, "onResize");
+    
     $(window).on("scroll", this.handleScroll);
     
-    this.resetCascadeContainer();
     this.articlesPerBatch = 8;
     
     this.allSubviews = [];
@@ -12,8 +13,22 @@ View.Article.Index.Main = Backbone.View.extend({
   
   
   resetCascadeContainer: function() {
-    this.hPosition = [0.0, 0.0, 0.0, 0.0];
-    this.vPosition = [0, 240, 480, 720];
+    this.maxWidth = this.$el.width();
+    this.columnWidth = 240;
+    this.columnCount = Math.floor(this.maxWidth / this.columnWidth);
+    if (this.columnCount < 1) {
+      this.columnCount = 1;
+    } else if (this.columnCount > 5) {
+      this.columnCount = 5;
+    }
+    this.actualWidth = this.columnCount * this.columnWidth;
+    
+    this.hPosition = [];
+    this.vPosition = [];
+    for (var index = 0; index < this.columnCount; ++index) {
+      this.hPosition.push(0.0);
+      this.vPosition.push(this.columnWidth * index);
+    }
     this.minHorizontalIndex = 0.0;
 
     this.batch = 0;
@@ -29,22 +44,30 @@ View.Article.Index.Main = Backbone.View.extend({
   
   
   render: function() {
-    this.$el.html(this.template());
+    var that = this;
     
-    var maxWidth = this.$el.width();
-    var actualWidth = Math.floor(maxWidth / 240) * 240;
-    $("#article-index-cascade-container").css("width", actualWidth + "px");
+    that.$el.html(that.template());
     
-    this.loadArticles();
+    that.maxWidth = that.$el.width();
+    that.resetCascadeContainer();
+
+    $("#article-index-cascade-container").css("width", that.actualWidth + "px");
     
-    return this;
+    that.loadArticles();
+    
+    $(window).on("resize", function() {
+      clearTimeout(that.resizeTimeout);
+      that.resizeTimeout = setTimeout(that.onResize,200);
+    });
+    
+    return that;
   },
   
   
   getMinHorizontalIndex: function() {
     this.minHorizontalIndex = 0.0;
     var min = this.hPosition[0];
-    for (var index = 1; index < 4; ++index) {
+    for (var index = 1; index < this.columnCount; ++index) {
       if (min > this.hPosition[index]) {
         min = this.hPosition[index];
         this.minHorizontalIndex = index;
@@ -106,6 +129,20 @@ View.Article.Index.Main = Backbone.View.extend({
   
   events: {
     "click #click_to_load": "loadArticles"
+  },
+  
+  
+  onResize: function() {
+    this.maxWidth = this.$el.width();
+    
+    if (Math.floor(this.maxWidth / this.columnWidth) !== this.columnCount) {
+      var that = this;
+      
+      that.$el.html(that.template());
+      that.resetCascadeContainer();
+      $("#article-index-cascade-container").css("width", that.actualWidth + "px");
+      that.loadArticles();
+    }
   },
   
   
