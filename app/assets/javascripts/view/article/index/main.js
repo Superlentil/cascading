34fc02022eead5e333635ad129b75e192939dfd2
Dingyu Zhou <dingyu.zhou@colorado.edu>
2014-08-21 00:34:20 -0700

    [Develop]: resize feature almost works.

--- app/assets/javascripts/view/article/index/main.js
+++ app/assets/javascripts/view/article/index/main.js
@@ -6,13 +6,15 @@ View.Article.Index.Main = Backbone.View.extend({
     
     $(window).on("scroll", this.handleScroll);
     
-    this.articlesPerBatch = 8;
+    this.articlesPerBatch = 1;
     
-    this.allSubviews = [];
+    this.batch = 0;
+    this.readyToLoad = true;
+    this.moreToLoad = true;
   },
   
   
-  resetCascadeContainer: function() {
+  resetCascade: function() {
     this.maxWidth = this.$el.width();
     this.columnWidth = 240;
     this.columnCount = Math.floor(this.maxWidth / this.columnWidth);
@@ -30,26 +32,23 @@ View.Article.Index.Main = Backbone.View.extend({
       this.vPosition.push(this.columnWidth * index);
     }
     this.minHorizontalIndex = 0.0;
-
-    this.batch = 0;
-    this.readyToLoad = true;
-    this.moreToLoad = true;
   },
   
   
   el: "#layout-content",
   
   
-  template: JST["template/article/index/main"],
+  mainTemplate: JST["template/article/index/main"],
+  coverTemplate: JST["template/article/index/cover"],
   
   
   render: function() {
     var that = this;
     
-    that.$el.html(that.template());
+    that.$el.html(that.mainTemplate());
     
     that.maxWidth = that.$el.width();
-    that.resetCascadeContainer();
+    that.resetCascade();
 
     $("#article-index-cascade-container").css("width", that.actualWidth + "px");
     
@@ -57,7 +56,7 @@ View.Article.Index.Main = Backbone.View.extend({
     
     $(window).on("resize", function() {
       clearTimeout(that.resizeTimeout);
-      that.resizeTimeout = setTimeout(that.onResize,200);
+      that.resizeTimeout = setTimeout(that.onResize, 300);
     });
     
     return that;
@@ -76,6 +75,17 @@ View.Article.Index.Main = Backbone.View.extend({
   },
   
   
+  getCascadeContainerHeight: function() {
+    var max = this.hPosition[0];
+    for (var index = 1; index < this.columnCount; ++index) {
+      if (max < this.hPosition[index]) {
+        max = this.hPosition[index];
+      }
+    }
+    return max;
+  },
+  
+  
   insertNewCoordinate: function(hCoordinate, vCoordinate) {
     this.hPosition[this.minHorizontalIndex] = hCoordinate;
     this.vPosition[this.minHorizontalIndex] = vCoordinate;
@@ -88,23 +98,30 @@ View.Article.Index.Main = Backbone.View.extend({
       
       var cascadeContainer = $("#article-index-cascade-container");
       var articles = new Collection.Article.Article();
+      
       articles.fetchBatch(that.batch, that.articlesPerBatch, {
         success: function(fetchedResults) {
           fetchedArticles = fetchedResults.models;
-          _.each(fetchedArticles, function(article) {
-            var viewArticleCover = new View.Article.Index.Cover({article: article});
-            that.allSubviews.push(viewArticleCover);   // prevent view memory leak
+          
+          _.each(fetchedArticles, function(article) {          
             that.getMinHorizontalIndex();
             var hCoordinate = that.hPosition[that.minHorizontalIndex];
             var vCoordinate = that.vPosition[that.minHorizontalIndex];
-            cascadeContainer.append(viewArticleCover.render(hCoordinate, vCoordinate).$el);
-            var coverHeight = viewArticleCover.$el.children("div.article_information").height() + article.get("cover_picture_height") + 32.0;
-            var newHorizontalCoordinate = 8.0 + hCoordinate + coverHeight;
+            
+            var articleCover = $("<div class='article-index-cover' style='top: " + hCoordinate + "px; left: " + vCoordinate + "px;'></div>");
+            articleCover.html(that.coverTemplate({article: article}));
+            cascadeContainer.append(articleCover);
+            
+            var articleCoverHeight = articleCover.outerHeight();
+            articleCover.attr("data-height", articleCoverHeight);
+            var newHorizontalCoordinate = 8.0 + hCoordinate + articleCoverHeight;
             that.insertNewCoordinate(newHorizontalCoordinate, vCoordinate);
+            
             if (newHorizontalCoordinate > cascadeContainer.height()) {
               cascadeContainer.css({"height": newHorizontalCoordinate + "px"});
             }
           });
+                    
           ++that.batch;
           that.readyToLoad = true;
           if (fetchedArticles.length < that.articlesPerBatch) {
@@ -118,7 +135,12 @@ View.Article.Index.Main = Backbone.View.extend({
   
   handleScroll: function(event) {
     var thisWindow = $(window);
-    if (thisWindow.scrollTop() + thisWindow.height() + 500 > $(document).height()) {
+    var scrollTopPosition = thisWindow.scrollTop();
+    var documentHeight = $(document).height();
+    
+    this.scrollPercentage = scrollTopPosition / documentHeight;
+    
+    if (scrollTopPosition + thisWindow.height() + 500 > documentHeight) {
       if (this.readyToLoad && this.moreToLoad) {
         this.readyToLoad = false;
         this.loadArticles();
@@ -137,25 +159,40 @@ View.Article.Index.Main = Backbone.View.extend({
     
     if (Math.floor(this.maxWidth / this.columnWidth) !== this.columnCount) {
       var that = this;
+           
+      var oldCascadeContainer = $("#article-index-cascade-container");
+      oldCascadeContainer.detach();
+      that.resetCascade();
       
-      that.$el.html(that.template());
-      that.resetCascadeContainer();
-      $("#article-index-cascade-container").css("width", that.actualWidth + "px");
-      that.loadArticles();
+      var newCascadeContainer = $("<div id='article-index-cascade-container' style='width: " + this.columnWidth * this.columnCount + "px;'></div>");
+      
+      oldCascadeContainer.children().each(function(index, articleCover) {
+        that.getMinHorizontalIndex();
+        var hCoordinate = that.hPosition[that.minHorizontalIndex];
+        var vCoordinate = that.vPosition[that.minHorizontalIndex];
+        
+        var jqueryArticleCover = $(articleCover);
+        jqueryArticleCover.css({"top": hCoordinate + "px", "left": vCoordinate + "px"});
+        newCascadeContainer.append(jqueryArticleCover);
+        
+        var newHorizontalCoordinate = 8.0 + hCoordinate + parseInt(jqueryArticleCover.attr("data-height"));
+        that.insertNewCoordinate(newHorizontalCoordinate, vCoordinate);
+        
+        if (newHorizontalCoordinate > newCascadeContainer.height()) {
+          newCascadeContainer.css({"height": newHorizontalCoordinate + "px"});
+        }
+      });
+      
+      that.$el.prepend(newCascadeContainer);
+      $(window).scrollTop($(document).height() * that.scrollPercentage);      
+      oldCascadeContainer.remove();
     }
   },
   
   
-  remove: function() {
-    var subview;
-    while (this.allSubviews.length > 0) {
-      subview = this.allSubviews.pop();
-      if (subview) {
-        subview.remove();
-      }
-    }
-    
+  remove: function() {   
     $(window).off("scroll");
+    $(window).off("resize");
     
     Backbone.View.prototype.remove.call(this);
   }
