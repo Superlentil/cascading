bae0346c8e0d0ec8477a483f155f4f0f0484a10b
Dingyu Zhou <dingyu.zhou@colorado.edu>
2014-10-26 12:24:34 -0700

    [Develop]: a little improvement for category page.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-  has_attached_file :cover_picture, :styles => {:medium => "400x300^"},
-    :convert_options => {:medium => "-gravity center -extent 400x300"},
+  has_attached_file :cover_picture, :styles => {:medium => "280x210^"},
+    :convert_options => {:medium => "-gravity center -extent 280x210"},
     :default_url => "/images/:style/missing.png",
     :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:id.:extension",
     :url => "/system/:class/:attachment/:id_partition/:style/:id.:extension"
