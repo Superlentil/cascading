9873a4fa64b225429322d0c05a6f2eb5be4a3a67
Dingyu Zhou <dzhou@newfieldwireless.com>
2014-10-28 18:23:10 -0700

    [Develop]: modified LoginSessionHelper in the back-end.

--- app/helpers/login_sessions_helper.rb
+++ app/helpers/login_sessions_helper.rb
@@ -1,13 +1,12 @@
 module LoginSessionsHelper
   def setUserLoginSession(user)
-    cookies[:user_id] = user.id
-    cookies[:user_email] = user.email
+    cookies[:user_id] = user.id.to_i
     cookies[:user_nickname] = user.nickname
     cookies[:user_avatar_url] = user.avatar.url(:thumb)
     cookies[:user_login_status] = "success"
     
-    session[:user_id] = user.id
-    session[:user_tier] = user.tier
+    session[:user_id] = user.id.to_i
+    session[:user_tier] = user.tier.to_i
   end
   
   
@@ -28,7 +27,7 @@ module LoginSessionsHelper
   
   
   def administrator?
-    return session.has_key?(:user_tier) && session[:user_tier].to_i == GlobalConstant::UserTier::ADMINISTRATOR
+    return session.has_key?(:user_tier) && session[:user_tier] == GlobalConstant::UserTier::ADMINISTRATOR
   end
   
   
@@ -38,6 +37,6 @@ module LoginSessionsHelper
   
   
   def correctLoggedInUser?(userId)
-    return session.has_key?(:user_id) && session[:user_id].to_i == userId.to_i
+    return session.has_key?(:user_id) && session[:user_id] == userId.to_i
   end
 end
