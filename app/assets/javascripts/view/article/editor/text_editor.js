23565429224048df6c6bf630f325337d2a639c69
Dingyu Zhou <dzhou@newfieldwireless.com>
2014-11-06 14:48:03 -0800

    [Develop]: be able to store and restore selection in article text editor.

--- app/assets/javascripts/view/article/editor/text_editor.js
+++ app/assets/javascripts/view/article/editor/text_editor.js
@@ -22,28 +22,32 @@ View.Article.Editor.TextEditor = View.Article.Editor.BaseEditor.extend({
       pasteText = window.clipboardData.getData("Text");
     }
     
+    var oldSelectRange = this.saveSelection();
+    
     if (pasteText.length === 0) {
       pasteText = prompt("Please paste here. Your browser may not support direct secure paste.") || "";
     }
 
     if (pasteText.length > 0) {
-      if (window.getSelection) {
-        var selection = window.getSelection();
-        if (selection.rangeCount) {
-          selection.getRangeAt(0).deleteContents();
-          selection.getRangeAt(0).insertNode(document.createTextNode(pasteText));
+      var restoredSelectRange = this.restoreSelection(oldSelectRange);
+      var pasted = false;
+      if (restoredSelectRange) {
+        if (window.getSelection) {
+          restoredSelectRange.deleteContents();
+          restoredSelectRange.insertNode(document.createTextNode(pasteText));
+          pasted = true;
+        } else if (document.selection) {
+          restoredSelectRange.text = pasteText;
+          pasted = true;
         }
-      } else if (document.selection) {
-        if (document.selection.type === "Text") {
-          var range = document.selection.createRange();
-          range.text = pasteText;
-        }
-      } else {
+      }
+
+      if (!pasted) {
         var input = $(event.currentTarget);
         input.html(input.html() + pasteText);
       }
     }
-  }
+  },
   
   
   // onBold: function(event) {
@@ -62,4 +66,38 @@ View.Article.Editor.TextEditor = View.Article.Editor.BaseEditor.extend({
       // }
     // }
   // }
+  
+  
+  saveSelection: function() {
+    if (window.getSelection) {
+      var selection = window.getSelection();
+      if (selection.getRangeAt && selection.rangeCount) {
+        return selection.getRangeAt(0);
+      }
+    } else {
+      var documentSelection = document.selection;
+      if (documentSelection && documentSelection.createRange) {
+        if (documentSelection.type === "Text") {
+          return documentSelection.createRange();
+        }
+      }
+    }
+    return null;
+  },
+  
+  
+  restoreSelection: function(oldSelectRange) {
+    if (oldSelectRange) {
+      if (window.getSelection) {
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(oldSelectRange);
+        return selection.getRangeAt(0);
+      } else if (document.selection && oldSelectRange.select) {
+        oldSelectRange.select();
+        return oldSelectRange;
+      }
+    }
+    return null;
+  }
 });
