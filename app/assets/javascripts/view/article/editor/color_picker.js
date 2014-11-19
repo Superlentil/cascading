34e90fa3d349f96fe3a7380670ab5895939cab5e
Dingyu Zhou <dzhou@newfieldwireless.com>
2014-11-18 17:52:32 -0800

    [Develop]: better color picker.

--- app/assets/javascripts/view/article/editor/color_picker.js
+++ app/assets/javascripts/view/article/editor/color_picker.js
@@ -13,6 +13,7 @@ View.Article.Editor.ColorPicker = Backbone.View.extend({
   
   render: function() {
     this.$el.html(this.template());
+    this.userColorInput = this.$el.find("input");
     return this;
   },
   
@@ -24,11 +25,52 @@ View.Article.Editor.ColorPicker = Backbone.View.extend({
   
   events: {
     "click pre": "selectColor",
+    "change input": "showUserColor",
+    "keyup input": "showUserColor",
+    "click button": "selectUserColor"
   },
   
   
   selectColor: function(event) {
+    event.prevetDefault();
     var color = $(event.currentTarget).data("color");
+    if (this.colorPickHandler) {
+      this.colorPickHandler(color);
+    }
+  },
+  
+  
+  getUserColorInput: function() {
+    var value = this.userColorInput.val() + "000";
+    var length = value.length;
+    if (length > 6) {
+      value = value.substr(0, 6);
+    } else if (length > 3) {
+      value = value.substr(0, 3);
+    }
+    var numArray = value.split("");
+    length = numArray.length;
+    
+    for (var index = 0; index < length; ++index) {
+      var num = numArray[index];
+      if ((num < '0' || num > '9') && ((num < 'a'|| num > 'f') && (num < 'A' || num > 'F'))) {
+        numArray[index] = '0';
+      }
+    }
+
+    return "#" + numArray.join("");
+  },
+  
+  
+  showUserColor: function(event) {
+    var color = this.getUserColorInput();
+    this.userColorInput.css("background-color", color);
+  },
+  
+  
+  selectUserColor: function(event) {
+    event.preventDefault();
+    var color = this.getUserColorInput();
     if (this.colorPickHandler) {
       this.colorPickHandler(color);
     }
