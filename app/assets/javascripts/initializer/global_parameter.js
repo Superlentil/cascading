26aa2f0a3af046fdd83ad6b2f82bb3d28b3e56fe
Dingyu Zhou <dingyu.zhou@colorado.edu>
2015-07-04 17:26:23 -0700

    [Develop]: test phonegap https

--- app/assets/javascripts/initializer/global_parameter.js
+++ app/assets/javascripts/initializer/global_parameter.js
@@ -5,7 +5,7 @@ var View = {};
 
 
 var GlobalConstant = {
-  DOMAIN: "http://cascading.pigrider.com",
+  DOMAIN: "https://cascading.pigrider.com",
 
   Article: {
     Status: {   // If this enumeration needs to be modified, please modify the corresponding one in the "Ruby on Rails" code together.
@@ -14,31 +14,31 @@ var GlobalConstant = {
       PRIVATE_PUBLISHED: 2,
       PUBLIC_PUBLISHED: 3
     },
-    
+
     SortBy: {   // If this enumeration needs to be modified, please modify the corresponding one in the "Ruby on Rails" code together.
       PUBLISH_TIME_DESC: 0,
       LOVE_DESC: 1,
       VIEW_DESC: 2
     },
-    
+
     CoverDisplayType: {
       NORMAL: 0,
       SEARCH_RESULT: 1
     }
   },
-    
-  
+
+
   SideNav: {
     BORDER_SHADOW_WIDTH_IN_PX: 10
   },
-  
-  
+
+
   User: {
     Tier: {   // If this enumeration needs to be modified, please modify the corresponding one in the "Ruby on Rails" code together.
       ADMINISTRATOR: 0,
       FREE_USER: 1
     },
-    
+
     Status: {
       UNVERIFIED: 0,
       VERIFIED: 1
@@ -57,35 +57,35 @@ var GlobalVariable = {
     WindowHeightInPx: 0,
     ScrollBarWidthInPx: 20
   },
-  
-  
+
+
   Layout: {
     ViewPopup: null,
-    
+
     SignInHandler: null,
     SignOutHandler: null,
     CloseNavHandler: null,
-    
+
     Header: {
       UpdateSubTitle: null,
       Hide: null,
       Show: null
     },
-    
+
     LeftNav: {
       FetchAllCategories: null
     }
   },
-  
-  
+
+
   Article: {
     AllCategories: null   // should be a Backbone Collection object
   },
-  
-  
+
+
   PageCache: {},
-  
-  
+
+
   UserCache: {},
 
 
@@ -107,8 +107,8 @@ var GlobalVariable = {
       this.UserAvatarUrl = null;
     }
   },
-  
-  
+
+
   BrowsingHistory: []
 };
 
